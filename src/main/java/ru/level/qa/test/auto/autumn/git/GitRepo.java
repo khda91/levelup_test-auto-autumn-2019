e9f9e9d00810503bea45750b7b0b08a1c965268f
Dmitry Khodakovsky <dmitrii.khodakovsky@clarivate.com>
2019-11-15 19:19:43 +0300

    Created GitRepo.java

---
+++ src/main/java/ru/level/qa/test/auto/autumn/git/GitRepo.java
@@ -0,0 +1,14 @@
+package ru.level.qa.test.auto.autumn.git;
+
+public class GitRepo {
+
+    private String repositoryName;
+
+    public GitRepo(String repositoryName) {
+        this.repositoryName = repositoryName;
+    }
+
+    public String getRepositoryName() {
+        return repositoryName;
+    }
+}
